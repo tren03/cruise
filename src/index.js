1423152888a6706ed0212a8935af90f16cadcb48
vishnu <vishnusethuraman@gmail.com>
2024-09-28 20:03:31 +0530

    history mode works, there was a bug with it not opeing tab in history mode, but i cannot seem to replicate it

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import Fuse from "fuse.js";
 const searchInput = document.getElementById("search");
 const resultsContainer = document.getElementById("results");
 const mainContainer = document.getElementsByClassName("main-container");
-const currentMode = document.getElementById("mode")
+const currentModeNode = document.getElementById("mode");
 
 const fuseOptions = {
     keys: ["title", "url"],
     threshold: 0.4,
 };
 
-let fuse;
+let fuseTab;
+let fuseHistory;
 let currentIndex = 0;
 let tabs = [];
+let history = [];
 let curwindowid;
 let isCurWindow;
+let currentMode = "T"
 
 // getting id of current window to check if we need to switch focus to another window
 let curwindow = await chrome.windows.getCurrent();
@@ -31,25 +34,40 @@ async function getTabs() {
                 isCurWindow = false;
             }
 
-            console.log(`Tab ${index + 1}:`);
-            console.log(`ID: ${tab.id}`);
-            console.log(`Title: ${tab.title}`);
-            console.log(`URL: ${tab.url}`);
-            console.log(`Window ID: ${tab.windowId}`);
-            console.log("is it in cur window" + isCurWindow);
-
-            console.log(`-----------------------------`);
+            /*           console.log(`Tab ${index + 1}:`);
+                       console.log(`ID: ${tab.id}`);
+                       console.log(`Title: ${tab.title}`);
+                       console.log(`URL: ${tab.url}`);
+                       console.log(`Window ID: ${tab.windowId}`);
+                       console.log("is it in cur window" + isCurWindow);
+           
+                       console.log(`-----------------------------`);*/
         });
 
-        fuse = new Fuse(tabs, fuseOptions);
-        renderDropDown(tabs, true); // Render all tabs initially
+        fuseTab = new Fuse(tabs, fuseOptions);
+        if (currentMode === "T") {
+            renderDropDown(tabs, true); // Render all tabs initially
+        }
     } catch (error) {
         console.error("Error fetching tabs: ", error);
     }
 }
 
-async function getHistory(){
-     
+async function getHistory() {
+    try {
+        history = await chrome.history.search({
+            text: "",
+        });
+
+        console.log("history array len " + history.length);
+        console.log("history fields " + JSON.stringify(history[0]));
+        fuseHistory = new Fuse(history, fuseOptions);
+        if (currentMode === "H") {
+            renderDropDown(history, true); // Render all tabs initially
+        }
+    } catch (err) {
+        console.log("problem in fetching history " + err);
+    }
 }
 
 // Rendering dropdown
@@ -57,7 +75,7 @@ function renderDropDown(filteredTabs, highlightMax) {
     resultsContainer.innerHTML = "";
     currentIndex = 0;
 
-    let limitedResults = filteredTabs.slice(0, 6); // Limit results to 7
+    let limitedResults = filteredTabs.slice(0, 6); // Limit results to 6
 
     // if no results, give option to search the web
     if (limitedResults.length == 0) {
@@ -72,27 +90,44 @@ function renderDropDown(filteredTabs, highlightMax) {
 
     limitedResults.forEach((tab, index) => {
         const li = document.createElement("li");
+        console.log("my current mode here "+currentMode)
 
         // Set the text to include both title and URL
+
         let inWindow = "";
-        if (tab.windowId != curwindowid) {
-            inWindow = " (other tab)";
+        if (currentMode === "T") {
+            if (tab.windowId != curwindowid) {
+                inWindow = " (other tab)";
+            }
         }
         li.innerHTML = `<strong>${tab.title}</strong><br><small>${tab.url}+${inWindow}</small>`;
         li.classList.add("result-item"); // Add a class for styling
         li.setAttribute("data-tab-id", tab.id); // Store tab ID
         li.setAttribute("data-search-element", false); // Store tab ID
-        li.setAttribute("data-window-id", tab.windowId);
+
+        if (currentMode === "T") {
+            li.setAttribute("data-window-id", tab.windowId);
+        }
 
         // Highlight the first result (highest score)
         if (highlightMax && index === 0) {
             li.classList.add("highlight"); // You can define this class in CSS to apply styles (like background color)
         }
 
-        // On click, switch to the clicked tab
-        li.addEventListener("click", () => {
-            chrome.tabs.update(tab.id, { active: true }); // Switch to the clicked tab
-        });
+        if (currentMode === "T") {
+            // On click, switch to the clicked tab
+            li.addEventListener("click", () => {
+                chrome.tabs.update(tab.id, { active: true }); // Switch to the clicked tab
+            });
+        }
+
+        if (currentMode === "H") {
+            console.log("added event lisner to hist data")
+            li.addEventListener("click", () => {
+                console.log("hist url : " + tab.url);
+                searchUrl(tab.url);
+            });
+        }
 
         resultsContainer.appendChild(li); // Append the list item to the unordered list
     });
@@ -111,15 +146,21 @@ function highlightItem(index) {
     });
 }
 
-async function searchUrl() {
+async function searchUrl(urlValue) {
     try {
-        let urlValue = searchInput.value;
-        // we use encoded uri component so that we can pass strings like "hwllo world" with spaces to the url, spaces get converted to %20
-        let finalUrlValue =`https://www.google.com/search?q=${encodeURIComponent(urlValue)}`; 
-        let create = await chrome.tabs.create({url:finalUrlValue});
-        console.log(create);
+        if (currentMode === "T") {
+            // we use encoded uri component so that we can pass strings like "hwllo world" with spaces to the url, spaces get converted to %20
+            let finalUrlValue = `https://www.google.com/search?q=${encodeURIComponent(urlValue)}`;
+            let create = await chrome.tabs.create({ url: finalUrlValue });
+            console.log(create);
+        }
+
+        if (currentMode === "H") {
+            let create = await chrome.tabs.create({ url: urlValue });
+            console.log(create);
+        }
     } catch (err) {
-        console.log("error in creating tab"+err);
+        console.log("error in creating tab" + err);
     }
 }
 
@@ -136,7 +177,7 @@ function handleNavigation(e) {
         items[0].click(); // Trigger click on the selected item
         console.log("you want to search the web");
 
-        searchUrl()
+        searchUrl(searchInput.value)
             .then(() => console.log("success in creating new tab"))
             .catch((err) => console.log("fail in creating new tab: " + err));
 
@@ -151,11 +192,13 @@ function handleNavigation(e) {
         highlightItem(currentIndex);
     } else if (e.key === "Enter" && currentIndex >= 0) {
         items[currentIndex].click(); // Trigger click on the selected item
-        let tabWindow = items[currentIndex].getAttribute("data-window-id");
-        if (tabWindow != curwindowid) {
-            focusWindow(tabWindow);
+        if (currentMode === "T") {
+            let tabWindow = items[currentIndex].getAttribute("data-window-id");
+            if (tabWindow != curwindowid) {
+                focusWindow(tabWindow);
+            }
+            console.log("the clicked tab has window id " + tabWindow);
         }
-        console.log("the clicked tab has window id " + tabWindow);
     }
 }
 
@@ -176,9 +219,27 @@ async function focusWindow(targetWindowId) {
 searchInput.addEventListener("input", (e) => {
     let query = e.target.value.trim();
     if (query === "") {
+        if(currentMode === "T" ){
+
         renderDropDown(tabs, true);
+        }
+
+        if(currentMode === "H" ){
+
+        renderDropDown(history, true);
+        }
     } else {
-        let fuzzres = fuse.search(query);
+        let fuzzres;
+        if (currentMode === "T") {
+            fuzzres = fuseTab.search(query);
+            console.log("render tab data")
+        }
+
+        if (currentMode === "H") {
+
+            console.log("render history data")
+            fuzzres = fuseHistory.search(query);
+        }
         let filteredTabs = fuzzres.map((result) => result.item);
         renderDropDown(filteredTabs, true); // Pass true to highlight the max score
     }
@@ -192,18 +253,19 @@ mainContainer[0].addEventListener("keydown", (e) => {
     }
 
     if (e.altKey && e.key === "h") {
+        renderDropDown(history, true);
         console.log("Alt + H pressed!");
-        currentMode.textContent = "H"
-
+        currentMode="H"
+        currentModeNode.textContent = "H";
     }
-    
-    
+
     if (e.altKey && e.key === "t") {
+        renderDropDown(tabs, true);
         console.log("Alt + T pressed!");
-        currentMode.textContent = "T" 
-
+        currentMode="T"
+        currentModeNode.textContent = "T";
     }
-
 });
 
+getHistory();
 getTabs();
